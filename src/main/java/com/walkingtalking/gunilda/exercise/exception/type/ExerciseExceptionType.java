cb75f361ee95fe97534bb7705b3f3d6b0a5061d2
Untaini <untaini@naver.com>
2024-05-21 16:46:52 +0900

    feat: 운동 데이터가 올바르지 않을 경우 400 response를 반환하는 기능 추가

--- src/main/java/com/walkingtalking/gunilda/exercise/exception/type/ExerciseExceptionType.java
+++ src/main/java/com/walkingtalking/gunilda/exercise/exception/type/ExerciseExceptionType.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum ExerciseExceptionType implements BaseExceptionType {
     EXERCISE_NOT_FOUND(HttpStatus.NOT_FOUND, "E001", "올바르지 않은 ID입니다."),
     EXERCISE_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "E002", "다른 사람의 운동 정보입니다."),
-    INSUFFICIENT_PARAMETER(HttpStatus.BAD_REQUEST, "E003", "파라미터 정보가 올바르지 않습니다.");
+    INSUFFICIENT_PARAMETER(HttpStatus.BAD_REQUEST, "E003", "파라미터 정보가 올바르지 않습니다."),
+    EXERCISE_CANNOT_SAVE(HttpStatus.BAD_REQUEST, "E004", "저장하려는 운동 데이터가 올바르지 않습니다.");
 
     final HttpStatus httpStatus;
     final String errorCode;
